50da4b084c51e4cf550409d3bc82f9b8d456b94c
Bohdankorzhynskyi <128472686+Bohdankorzhynskyi@users.noreply.github.com>
2023-05-01 15:01:27 +0300

    Add files via upload

---
+++ mn.py
@@ -0,0 +1,8 @@
+from mp import number_to_words
+def m_n():
+    number=1
+    while number!=0:
+      number = int(input("Введіть номер: "))
+      words = number_to_words.convert(number)
+      print(f"Ви ввели: {words}")
+m_n()
